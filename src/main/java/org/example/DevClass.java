2ec1e79731a6e3d94a40d8a3b8e8189b5ec9fec8
krishnamohank2726 <168171556+krishnamohank2726@users.noreply.github.com>
2024-04-26 15:57:49 +0530

    Update DevClass.java new

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Git Learning");
+        System.out.println("DevClass new Changes");
     }
 }
