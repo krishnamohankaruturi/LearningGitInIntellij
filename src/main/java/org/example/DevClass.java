9bb8efcba9d5cabfba1a015043dcd4dec6878ecc
krishnamohank2726 <168171556+krishnamohank2726@users.noreply.github.com>
2024-04-26 15:02:12 +0530

    Update DevClass.java

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Git Learning");
+        System.out.println("Stream to learn");
+        
     }
 }
