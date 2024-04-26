7e9573c3ef3d9377f90af75aa97d931d099ef17a
krishnamohankaruturi <krishnamohank1001@gmail.com>
2024-04-26 16:17:42 +0530

    new changes in feature branch

--- src/main/java/org/example/DevClass.java
+++ src/main/java/org/example/DevClass.java
@@ -3,6 +3,7 @@ package org.example;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Stream to learn remove + from code and then squash and commit");
+        System.out.println("new changes"+ "Krishnamohan KAruturi");
         
     }
 }
